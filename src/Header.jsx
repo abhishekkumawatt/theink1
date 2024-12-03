5001fc58cd9115e1e6d26e8031d0e69f9ffce249
abhishekkumawatt <kumawatabhi27@gmail.com>
2024-12-03 17:01:18 +0530

    Initial commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,6 +12,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { IoSearchSharp } from "react-icons/io5";
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 export const Header = () => {
   return (
     <div className=''>
@@ -35,13 +39,14 @@ export const Header = () => {
           </Dropdown>
         </div>
       </div>
-      <div  className='logonavbar'>
+
+      <div  className='logonavbar px-5 w-100 justify-content-between align-items-center'>
+      {/* <div className='navbar'>
+      <Navbar>
+      <Container>
       <div className='theinklogo'>
       <Navbar.Brand><Link className='NvaLink' to='/'><img src={logo}/></Link></Navbar.Brand>
       </div>
-      <div className='navbar'>
-      <Navbar>
-      <Container>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Link className='navlink' to="/" style={{textDecoration:'none', color:'black', margin:'.5rem 0 0 1rem'}}>HOME</Link>
@@ -56,10 +61,37 @@ export const Header = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+      </div> */}
+
+<Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+      <div className='theinklogo'>
+      <Navbar.Brand><Link className='NvaLink' to='/'><img src={logo}/></Link></Navbar.Brand>
       </div>
-      <div className='searchicon'>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link className='navlink' to="/" style={{textDecoration:'none', color:'black', margin:'.5rem 0 0 1rem'}}>HOME</Link>
+            <Nav.Link href="#education">EDUCATION</Nav.Link>
+            <Nav.Link href="#politics">POLITICS</Nav.Link>
+            <Nav.Link href="#sports">SPORTS</Nav.Link>
+            <Nav.Link href="#travel">TRAVEL</Nav.Link>
+            <Nav.Link href="#art">ART</Nav.Link>
+            <Nav.Link href="#state">STATE</Nav.Link>            
+          </Nav>
+          <div className='searchicon'>
       <IoSearchSharp />
       </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+     
+      
     </div>
 
     </div>
--- src/Privacy Policy/Privacy.js
+++ src/Privacy Policy/Privacy.js
@@ -1,140 +1,61 @@
-import React from "react";
-import "./Privacy.css";
-import { Header } from "../Header";
-import Footer from "../Footer/Footer";
+// import React from 'react'
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+// import Form from 'react-bootstrap/Form';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Privacy = () => {
-  return (
-    <div className="privacy-policy">
-      <Header />
-      <div className="privacy-box fluid-container">
-        <div className="privacy-paragraph-header ">
-          <h1><b>Privacy Policy</b></h1>
-        </div>
-        <div className="privacy-data">
-          <h6>
-            Theink understands the importance of your privacy and are committed
-            to protecting it at all times.This Mobile Privacy Policy applies to
-            users of ‘Theink mobile App’ through Theink This Policy describes
-            how the ‘Theink mobile App’ may collect, use and share information
-            from or about you, and explains how the information may be shared or
-            used.
-          </h6>
-          <br />
-          <br />
-          <h6>
-            Agreement to Policy : By installing the ‘Theink mobile App’ on a
-            mobile device, you consent to this Policy which includes your
-            consent to disclose and use information about you in the manner
-            detailed in this Policy.
-          </h6>
-          <h6>
-            Gathering, Using and Sharing: Information that we collect :<br />
-            <br />
-            All Information collected shall only be used to provide the user
-            with the best possible services
-          </h6>
-          <h6>
-            From time to time, we will use your information to provide you with
-            improved services and information about additional services, as well
-            as to comply with applicable laws and regulations – but we will
-            always use that information responsibly.
-          </h6>
-          <h6>
-            The information that we may collect about you through ‘Theink mobile
-            App’ includes information that you voluntarily disclose, such as
-            your name, address, phone number, email address and other contact
-            information; your location information.
-          </h6>
-          <h6>
-            We can track your logins to help diagnose problems with our server
-            and/or to administer our ‘Theink Mobile App’. We maintain procedures
-            to ensure accurate information and to respond in a timely manner to
-            your requests to change or correct information.This gives us an idea
-            of which parts of our application users are visiting and facing
-            issues. We do not link IP addresses to anything personally
-            identifiable.
-          </h6>
-          <h6>
-            Security Measures: <br /> <br /> The Information shall not be shared
-            with any external organisation . This application has security
-            measures in place to protect the loss, misuse, and/or alteration of
-            information under our control. The data resides behind a firewall,
-            we protect the information you provide us in several ways as the
-            database is secured in our own servers. Permission is not provided
-            to third party application to read the user data.
-          </h6>
-          <h6>
-            User IDs and Passwords are used to help safeguard access to your
-            information through the ‘Theink mobile App’. As always, we strongly
-            encourage you to assist us in that effort by not sharing your User
-            IDs and Passwords with anyone.
-          </h6>
-          <h6>
-            Questions and Concerns <br />
-            If you have any questions about this privacy statement, the
-            practices of this application, or your dealings with this
-            application, you can contact Franciscan, please contact us by
-            submitting your question via the Customer Service Contact Us Form at
-            www.Theink.in
-          </h6>
-          <h6>
-            Policy Updates and Effective Date
-            <br />
-            <br />
-            This Policy is subject to change and any changes to this Policy will
-            become effective when posted on this application. Your use of the
-            application following these changes means you accept the revised
-            Policy.
-            <br />
-            Camera
-            <br />
-            Camera is required in order to click the photographs of the content
-            you need to put back on application for further communication with
-            stakeholders like as assignment, profile photo, homework etc.
-            <br />
-            All media access
-            <br />
-            All media access is required in order to upload the photographs of
-            the content you for further communication with stakeholders like as
-            assignment, profile photo, homework etc which would be available the
-            users gallery..
-          </h6>
-          <h6>
-            Location
-            <br />
-            <br />
-            We need permission to access your location to ensure the security of
-            your app been misused.
-            <br />
-            <br />
-            Theink also may disclose information about you as permitted or
-            required by law.
-            <br />
-            <br />
-            At Theink , we value your relationship and will at all times strive
-            to ensure your privacy.
-            <br />
-            <br />
-            <br />
-            Data collected by app
-            <br />
-            <br />
-            Name
-            <br />
-            Email
-            <br />
-            Mobile Number
-            <br />
-            Location Information
-            <br />
-            Camera Access
-          </h6>
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
-};
 
-export default Privacy;
+// const practice = () => {
+//   return (
+//     <div className='header'>
+      
+//     <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container fluid>
+//         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="navbarScroll" />
+//         <Navbar.Collapse id="navbarScroll">
+//           <Nav
+//             className="me-auto my-2 my-lg-0"
+//             style={{ maxHeight: '100px' }}
+//             navbarScroll
+//           >
+//             <Nav.Link href="#action1">Home</Nav.Link>
+//             <Nav.Link href="#action2">Link</Nav.Link>
+//             <Nav.Link href="#action1">Home</Nav.Link>
+//             <Nav.Link href="#action2">Link</Nav.Link>
+//             <Nav.Link href="#action1">Home</Nav.Link>
+//             <Nav.Link href="#action2">Link</Nav.Link>
+//             <NavDropdown title="Link" id="navbarScrollingDropdown">
+//               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action4">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action5">
+//                 Something else here
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//             <Nav.Link href="#" disabled>
+//               Link
+//             </Nav.Link>
+//           </Nav>
+//           <Form className="d-flex">
+//             <Form.Control
+//               type="search"
+//               placeholder="Search"
+//               className="me-2"
+//               aria-label="Search"
+//             />
+//             <Button variant="outline-success">Search</Button>
+//           </Form>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+    
+//     </div>
+//   )
+// }
+
+// export default practice
